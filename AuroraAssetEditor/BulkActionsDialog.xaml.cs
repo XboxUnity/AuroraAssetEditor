aa1e3f13290a75f7eb9e9fe88ff22a6f8c358e7c
Craig Huggins <craighuggins@hotmail.com>
2025-08-23 21:26:44 +1000

    Added ability to bulk download assets from internet archive for cover art and xbox.com for other assets.

---
+++ AuroraAssetEditor/BulkActionsDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿// 
+//  BulkActionsDialog.xaml.cs
+//  AuroraAssetEditor
+// 
+//  Created by Cehbab on 23/08/2025
+//  Copyright (c) 2015 Swizzy. All rights reserved.
+
+namespace AuroraAssetEditor {
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public partial class BulkActionsDialog
+	{
+        public BulkActionsDialog(Window owner) {
+            InitializeComponent();
+            Icon = App.WpfIcon;
+            Owner = owner;
+        }
+
+		public bool ReplaceExisting { get { return ReplaceExistingChk.IsChecked ?? false; } }
+
+		public bool CoverArtOnly { get { return CoverArtOnlyChk.IsChecked ?? false; } }
+
+		private void btnDialogOk_Click(object sender, RoutedEventArgs e) { DialogResult = true; }
+
+        private void OnTextInput(object sender, TextCompositionEventArgs e) {
+            uint tmp;
+            e.Handled = !uint.TryParse(e.Text, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out tmp);
+        }
+	}
+}
